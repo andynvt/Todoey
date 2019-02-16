6b878cbee31f6dd521d5b5119420e0d58ba5e67a
ANDY <andy.nvt.vn@gmail.com>
2019-02-16 11:01:41 +0700

    convert to MCV design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by ANDY on 2/15/19.
+//  Copyright © 2019 ANDY. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
